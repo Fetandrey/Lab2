60bbedf50329e9227edf91db8c91134d5409d763
Fetandrey <fetandrey@outlook.com>
2016-10-13 21:03:17 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text:");
+            string text = Console.ReadLine();
+            char[] separators = {' ',',','.',':',';',}; //Can be expanded
+            string[] words = text.Split(separators); //Leaves "" between separators
+            Dictionary<string, int> matches = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if(!matches.ContainsKey(word))
+                {
+                    matches.Add(word, 1);
+                }
+                else
+                {
+                    matches[word]++;
+                }
+            }
+            foreach (string word in matches.Keys)
+            {
+                if(word != "")
+                {
+                    Console.WriteLine("Word: {0}\toccurs in this text {1} times", word, matches[word]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+//hello,hello bye, bye; hello
